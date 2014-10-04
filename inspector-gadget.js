7c3a1e9d14f9a0a2a02d3653f9565c66c1467843
Doug Turnbull <softwaredoug@gmail.com>
2014-10-04 08:48:56 -0400

    fix indentation

--- inspector-gadget.js
+++ inspector-gadget.js
@@ -62,90 +62,90 @@ angular.module('swd.inspector-gadget')
       var uniqueDirective = 1;
 
       var link = function(scope, element, attrs) {
-      var anchoredDiv = element.find('.anchored_div');
-      var root = rootNode(anchoredDiv);
+        var anchoredDiv = element.find('.anchored_div');
+        var root = rootNode(anchoredDiv);
 
-      var myPopoverId = scope._insp.popoverId;
-      var titleHtml = scope._insp.titleHtml;
-      var contentHtml = scope._insp.contentHtml;
+        var myPopoverId = scope._insp.popoverId;
+        var titleHtml = scope._insp.titleHtml;
+        var contentHtml = scope._insp.contentHtml;
 
-      var popConfig = collectPopoverAttrArgs(attrs);
-      var popControl = null;
+        var popConfig = collectPopoverAttrArgs(attrs);
+        var popControl = null;
 
-      var timeoutObj = null;
-      
-      popConfig.html = true;
-      popConfig.title = function() {
-        return titleHtml;
-      };
-      popConfig.content = function() {
-        return contentHtml;
-      };
-      popConfig.trigger = 'manual';
-      anchoredDiv.mouseenter(function() {
-        // trigger hover event, open popover, link btn to modal
-        popControl.show();
-        // TODO remaining problem -- this compiles 
-        var popCont = getPopoverContainer(root, myPopoverId);
-        $compile(popCont.contents())(scope);
-        scope.$apply();
-      });
-      anchoredDiv.mouseleave(function() {
-        /* only leave if hoverd off content too*/
-        var popCont = getPopoverContainer(root, myPopoverId);
-        var hoveringPopover = false;
-
-        popCont.mouseenter(function() {
-          hoveringPopover = true;
-        });
-        popCont.mouseleave(function() {
-          hoveringPopover = false;
-        });
-
-        var hidePop = function() {
-          if (!hoveringPopover) {
-            popControl.hide();
-          } else {
-            timeoutObj = $timeout(hidePop, 300);
-          }
+        var timeoutObj = null;
+        
+        popConfig.html = true;
+        popConfig.title = function() {
+          return titleHtml;
         };
+        popConfig.content = function() {
+          return contentHtml;
+        };
+        popConfig.trigger = 'manual';
+        anchoredDiv.mouseenter(function() {
+          // trigger hover event, open popover, link btn to modal
+          popControl.show();
+          // TODO remaining problem -- this compiles 
+          var popCont = getPopoverContainer(root, myPopoverId);
+          $compile(popCont.contents())(scope);
+          scope.$apply();
+        });
+        anchoredDiv.mouseleave(function() {
+          /* only leave if hoverd off content too*/
+          var popCont = getPopoverContainer(root, myPopoverId);
+          var hoveringPopover = false;
 
-        timeoutObj = $timeout(hidePop, 300);
-      });
-      popControl = intPopover.bootstrap(anchoredDiv, popConfig);
-      
-    };
-    return {
-      restrict: 'E',
-      transclude: true,
-      //template: '<div ng-transclude></div>',
-
-      compile: function(elem, attrs, transclude) {
-        return function(scope, /*out*/lElem /*out lAttrs*/) {
-          var childScope = scope.$new();
-          transclude(childScope, function(clone, innerScope) {
-            innerScope._insp = {};
-            var div = angular.element('<div class="anchored_div" style="display: inline-block"></div>');
-            angular.forEach(clone, function(c) {
-              if (c.nodeType !== 1 || (c.tagName !== 'INSPECTOR-TITLE' && c.tagName !== 'INSPECTOR-CONTENT')) {
-                div.append(c);
-              }
-              else if (c.nodeType === 1 && c.tagName === 'INSPECTOR-CONTENT') {
-                innerScope._insp.contentHtml = c.innerHTML;
-              }
-              else if (c.nodeType === 1 && c.tagName === 'INSPECTOR-TITLE') {
-                var myPopoverId = uniqueDirective++;
-                innerScope._insp.popoverId = myPopoverId;
-                innerScope._insp.titleHtml = tagHtml(c, myPopoverId);
-              }
-            });
-            lElem.append(div);
+          popCont.mouseenter(function() {
+            hoveringPopover = true;
           });
-          link(childScope, elem, attrs);
-        };
-      },
-    };
-  });
+          popCont.mouseleave(function() {
+            hoveringPopover = false;
+          });
+
+          var hidePop = function() {
+            if (!hoveringPopover) {
+              popControl.hide();
+            } else {
+              timeoutObj = $timeout(hidePop, 300);
+            }
+          };
+
+          timeoutObj = $timeout(hidePop, 300);
+        });
+        popControl = intPopover.bootstrap(anchoredDiv, popConfig);
+        
+      };
+      return {
+        restrict: 'E',
+        transclude: true,
+        //template: '<div ng-transclude></div>',
+
+        compile: function(elem, attrs, transclude) {
+          return function(scope, /*out*/lElem /*out lAttrs*/) {
+            var childScope = scope.$new();
+            transclude(childScope, function(clone, innerScope) {
+              innerScope._insp = {};
+              var div = angular.element('<div class="anchored_div" style="display: inline-block"></div>');
+              angular.forEach(clone, function(c) {
+                if (c.nodeType !== 1 || (c.tagName !== 'INSPECTOR-TITLE' && c.tagName !== 'INSPECTOR-CONTENT')) {
+                  div.append(c);
+                }
+                else if (c.nodeType === 1 && c.tagName === 'INSPECTOR-CONTENT') {
+                  innerScope._insp.contentHtml = c.innerHTML;
+                }
+                else if (c.nodeType === 1 && c.tagName === 'INSPECTOR-TITLE') {
+                  var myPopoverId = uniqueDirective++;
+                  innerScope._insp.popoverId = myPopoverId;
+                  innerScope._insp.titleHtml = tagHtml(c, myPopoverId);
+                }
+              });
+              lElem.append(div);
+            });
+            link(childScope, elem, attrs);
+          };
+        },
+      };
+    });
 
 angular.module('swd.inspector-gadget')
   .directive('inspectorContent', function() {
